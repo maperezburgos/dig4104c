0f6ad06acbfd1e96dac7a76a4c9cd4a4fa8376ee
dominguez-m <46730527+dominguez-m@users.noreply.github.com>
2019-04-16 09:56:25 -0400

    progress on about page

---
+++ about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <!-- Site Properties -->
+    <title>Clusterizr | About</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+  </head>
+  <body>
+    <?php  include 'header.php';?>
+    <div class="ui inverted vertical center aligned">
+      <img src="https://via.placeholder.com/600x200" alt="banner" class="banner" width="100%" height="auto">
+      </div>
+
+  <div class="ui vertical stripe segment">
+    <div class="ui middle aligned stackable grid container">
+      <h1 class="ui center aligned header">About Clusterizr</h1>
+      <div class="row">
+        <div class="six wide column">
+          <img class="ui medium circular image" src="/images/wireframe/square-image.png">
+        </div>
+          <div class="eight wide right floated column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dui felis, semper ut consequat a, gravida quis enim. Morbi et condimentum velit. Sed gravida lacus id magna eleifend vestibulum. Ut sodales lacus sed libero convallis, in laoreet sem commodo. Donec sollicitudin massa diam, sit amet tempor est ullamcorper ac. Ut nec metus at mauris placerat bibendum. Praesent iaculis malesuada elit ut congue. Mauris gravida massa nibh, a hendrerit tellus finibus eu. Ut et massa a sem pharetra gravida. Praesent placerat pharetra purus non porta.</p>
+          </div>
+        </div>
+        <div class="row">
+      </div>
+    </div>
+  </div>
+
+
+
+      <div class="ui segment center">
+
+      </div>
+  </body>
+</html>
